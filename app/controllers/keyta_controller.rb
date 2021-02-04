6fa197d18d8e3848a491c0ad9c26bea0da704bc7
Fadli <mnurfadd@gmail.com>
2021-02-04 22:37:54 +0700

    Added second answer

--- app/controllers/keyta_controller.rb
+++ app/controllers/keyta_controller.rb
@@ -11,6 +11,25 @@ class KeytaController < ApplicationController
 
     render json: { data: @data[0] }, status: :ok
   end
+
+  def numbertwo
+    if @data[0]["results"][0]["services"] != []
+      temp = @data[0]["results"][0]["services"][0]["totalPrice"]
+      markup = @data[0]["results"][0]["services"][0]["totalPrice"]
+      if temp >= 0 && temp <= 17000
+        @data[0]["results"][0]["services"][0]["hargaMarkup"] = markup + 1000
+      elsif temp >= 17001 && temp <= 30000
+        @data[0]["results"][0]["services"][0]["hargaMakrup"] = markup + 2000
+      elsif temp >= 30001 && temp <= 40000
+        @data[0]["results"][0]["services"][0]["hargaMarkup"] = markup + 3000
+      elsif temp >= 40001 && temp <= 129000
+        @data[0]["results"][0]["services"][0]["hargaMarkup"] = markup + 5000
+      elsif temp >= 129001
+        @data[0]["results"][0]["services"][0]["hargaMarkup"] = markup + 7000
+      end
+    end
+    render json: { data: @data[0] }, status: :ok
+  end
   
   def getdata
     @helper = Helper.new
